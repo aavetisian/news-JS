146ebbd530f632961a1bae54597f352c27604075
Ani <aavetisian@noorlogic.com>
2023-06-08 17:18:53 +0400

    init: start project

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ab0de0ecd79d494d8d10d57878050981', // получите свой ключ https://newsapi.org/
         });
     }
 }
